506da5ab13a06eeb9c6af568a4214cbe399a9a61
ram125 <ram.123.guitar@gmail.com>
2024-07-12 15:49:08 +0300

    got the communication between clients work

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,21 @@ import './App.css';
 import { useState, useEffect } from "react";
 
 function App() {
-  const [isClicked, setClicked] = useState(false);
+  const [isClicked, setClicked] = useState(0); //0 not loaded; 1 clicked, red; 2 not clicked
+  const [socketState, setSState] = useState(null);
   useEffect(() => {
     const socket = new WebSocket('ws://localhost:5000');
-
+    setSState(socket);
     socket.onopen = () => {
       socket.send(JSON.stringify({
+        type: 'greet',
         msg: 'sent u message from client'
       }));
     };
-
     socket.onmessage = (event) => {
       console.log(event.data);
+      let messageObject = JSON.parse(event.data);
+      setClicked(messageObject.isClicked);
     };
     return () => {
       if (socket.readyState === WebSocket.OPEN) {
@@ -22,14 +25,31 @@ function App() {
       }
     };
   }, []);
+
+  const getClasses = (isClicked) => {
+    if (isClicked === 1) return 'mainbutton clicked';
+    if (isClicked === 2) return 'mainbutton notclicked';
+    return 'mainbutton';
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <div className={`mainbutton ${isClicked ? "clicked" : "notclicked"}`} onClick={() => {
-            if (isClicked) {
-                setClicked(false);
-            } else {
-                setClicked(true);
+        <div className={`${getClasses(isClicked)}`} onClick={() => {
+            if (isClicked === 1) {
+              if (socketState && socketState.readyState === WebSocket.OPEN) {
+                socketState.send(JSON.stringify({
+                  type: 'updateState',
+                  isClicked: 2
+                }));
+              }
+            } else if(isClicked === 2) {
+              if (socketState && socketState.readyState === WebSocket.OPEN) {
+                socketState.send(JSON.stringify({
+                  type: 'updateState',
+                  isClicked: 1
+                }));
+              }
             }
         }}>click me to change the color</div>
       </header>
--- server/index.js
+++ server/index.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const app = express();
 
-const WSSserver = require('express-ws')(app)
+const WSSserver = require('express-ws')(app);
+const aWss = WSSserver.getWss();
 
 const PORT = process.env.PORT || 5000;
 
+let isClicked = 2;
 app.ws('/', (ws, req) => {
-    ws.send('sent from server')
+    ws.send(JSON.stringify({
+        type: 'greet',
+        isClicked: isClicked
+      })); 
     ws.on('message', (msg)=>{
-        console.log(msg);
+        let messageObject = JSON.parse(msg);
+        if(messageObject.type === 'updateState'){
+            isClicked = messageObject.isClicked;
+            broadcastNewState(messageObject.isClicked)
+        }else if(messageObject.type === 'greet'){
+            ws.send(JSON.stringify({
+                type: 'greet',
+                isClicked: isClicked
+              })); 
+        }
     })
 })
+
+
+const broadcastNewState = (state) => {
+    aWss.clients.forEach(client => {
+        client.send(JSON.stringify({
+            type: 'updateState',
+            isClicked: state
+          })); 
+    });
+}
 
 app.listen(PORT, ()=>console.log(`the server is started on port ${PORT}`));
