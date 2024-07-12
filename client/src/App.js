56dc6d53125bc5d5906ca383ba0e1f037e62cb33
ram125 <ram.123.guitar@gmail.com>
2024-07-12 12:20:37 +0300

    did the base for the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [isClicked, setClicked] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className={`mainbutton ${isClicked ? "clicked" : "notclicked"}`} onClick={() => {
+            if (isClicked) {
+                setClicked(false);
+            } else {
+                setClicked(true);
+            }
+        }}>click me to change the color</div>
       </header>
     </div>
   );
